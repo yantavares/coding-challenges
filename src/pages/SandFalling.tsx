0b718b69486504bd7bee5813c3affb0f7a720724
yantavares <yantdo1@gmail.com>
2024-02-01 20:34:39 -0300

    made sand different colors

--- src/pages/SandFalling.tsx
+++ src/pages/SandFalling.tsx
@@ -2,31 +2,36 @@ import React, { useEffect, useRef } from "react";
 import p5 from "p5";
 import { initializeGrid } from "./utils";
 
-const black = 50;
-const white = 255;
-const w = 10;
+const black = 0;
+const w = 8;
 
 const SandFalling = () => {
   const sketchRef = useRef<HTMLDivElement>(null);
   let grid: number[][] = [];
 
+  // Adjust hueValue to cycle within a yellow range
+  let hueValue = 50;
+  const yellowMin = 50;
+  const yellowMax = 70;
+
   useEffect(() => {
     const sketch = (p: p5) => {
       p.setup = () => {
         p.createCanvas(680, 680);
-        p.frameRate(80);
+        p.frameRate(30);
+        p.colorMode(p.HSB, 360, 255, 255);
         const cols = Math.floor(p.width / w);
         const rows = Math.floor(p.height / w);
-        grid = initializeGrid(cols, rows, black);
+        grid = initializeGrid(cols, rows);
       };
 
       p.draw = () => {
         p.background(black);
-
         grid.forEach((row, rowIndex) => {
           row.forEach((cell, colIndex) => {
             p.stroke(black);
-            p.fill(cell);
+            if (cell === black) p.fill(black);
+            else p.fill(cell, 255, 255);
             p.square(colIndex * w, rowIndex * w, w);
           });
         });
@@ -35,25 +40,25 @@ const SandFalling = () => {
         let updatedGrid = grid.map((row) => [...row]);
         for (let i = 0; i < grid.length - 1; i++) {
           for (let j = 0; j < grid[i].length; j++) {
-            if (grid[i][j] === white) {
+            if (grid[i][j] !== black) {
               if (grid[i + 1][j] === black) {
                 // below is empty
                 updatedGrid[i][j] = black;
-                updatedGrid[i + 1][j] = white;
+                updatedGrid[i + 1][j] = hueValue;
               } else if (
                 // below left is empty
                 grid[i + 1][j - 1] === black &&
                 grid[i][j - 1] === black
               ) {
                 updatedGrid[i][j] = black;
-                updatedGrid[i + 1][j - 1] = white;
+                updatedGrid[i + 1][j - 1] = hueValue;
               } else if (
                 // below right is empty
                 grid[i + 1][j + 1] === black &&
                 grid[i][j + 1] === black
               ) {
                 updatedGrid[i][j] = black;
-                updatedGrid[i + 1][j + 1] = white;
+                updatedGrid[i + 1][j + 1] = hueValue;
               }
             }
           }
@@ -73,16 +78,22 @@ const SandFalling = () => {
           const row = Math.floor(p.mouseY / w);
           const area = 4;
           const limit = Math.floor(area / 2);
-          if (grid[row] && grid[row][col] !== undefined) {
+          if (
+            grid[row] &&
+            grid[row][col] !== undefined &&
+            grid[row][col] === black
+          ) {
             for (let i = -limit; i < limit; i++) {
               for (let j = -limit; j < limit; j++) {
                 if (grid[row + i] && grid[row + i][col + j] !== undefined) {
-                  if (p.random(1) < 0.5) grid[row + i][col + j] = white;
+                  if (p.random(1) < 0.5) grid[row + i][col + j] = hueValue;
                 }
               }
             }
           }
         }
+        hueValue = hueValue + 0.5;
+        if (hueValue > yellowMax) hueValue = yellowMin;
       };
 
       // Mouse pressed event
