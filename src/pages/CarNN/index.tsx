515d4d04852e0cefd2cea4f774c9a7ed533f3c74
yantavares <yantdo1@gmail.com>
2024-02-27 15:35:11 -0300

    Refactor CarNN component to generate multiple AI cars

--- src/pages/CarNN/index.tsx
+++ src/pages/CarNN/index.tsx
@@ -20,22 +20,35 @@ const CarNN = () => {
       const networkCtx = networkCanvas.getContext("2d");
 
       const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-      const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
+
+      const cars = generateCars(100);
+
       const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "NPC", 2)];
 
       animate();
+
+      function generateCars(n) {
+        const cars = [];
+        for (let i = 0; i < n; i++) {
+          cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
+        }
+        return cars;
+      }
 
       function animate(time = null) {
         traffic.forEach((car) => {
           car.update(road.borders);
         });
-        car.update(road.borders, traffic);
+
+        for (let i = 0; i < cars.length; i++) {
+          cars[i].update(road.borders, traffic);
+        }
 
         carCanvas.height = window.innerHeight;
         networkCanvas.height = window.innerHeight;
 
         carCtx.save();
-        carCtx.translate(0, -car.y + window.innerHeight * 0.7);
+        carCtx.translate(0, -cars[0].y + window.innerHeight * 0.7);
 
         road.draw(carCtx);
 
@@ -43,12 +56,18 @@ const CarNN = () => {
           car.draw(carCtx, "red");
         });
 
-        car.draw(carCtx);
+        carCtx.globalAlpha = 0.2;
+
+        for (let i = 0; i < cars.length; i++) {
+          cars[i].draw(carCtx, "blue");
+        }
+
+        carCtx.globalAlpha = 1;
 
         carCtx.restore();
         networkCtx.lineDashOffset = -time / 100;
 
-        Visualizer.drawNetwork(networkCtx, car.brain);
+        Visualizer.drawNetwork(networkCtx, cars[0].brain);
 
         requestAnimationFrame(animate);
       }
